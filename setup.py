575115272c6d89dc6c59ea71b78d77441ce55d47
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2021-08-19 15:48:57 +0200

    Bump version to deploy

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ extra_required["plot"].remove("-r requirements.txt")
 
 setuptools.setup(
     name="tsp-solvers",
-    version="0.0.2a",
+    version="0.0.3",
     author="Guillermo González-Santander",
     author_email="g.gsantanderdelacruz@gmail.com",
     description="Set of different methods to solve the Travelling Salesman Problem. Each method has its own class",
